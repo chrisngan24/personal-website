9bef035f8de6a43395cd99b7b90ef8d751b1db39
Chris Ngan <chris.ngan24@gmail.com>
2013-08-04 00:25:13 -0400

    added custom bootstrap to fix row issues

---
+++ static/index.php
@@ -0,0 +1,21 @@
+<?php
+
+	
+	<html>
+		<head>
+			<!-- <link rel="stylesheet" type="text/css" href="static/css/main.css"> -->
+			<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+			<!--<script type="text/javascript" src="libs/jquery-1.10.1.min.js"></script>-->
+			<script type="text/javascript" src="libs/jquery-1.10.2.min.js"></script>
+			<script type="text/javascript" src="libs/modernizr.custom.74850.js"></script>
+			<script type="text/javascript" src="js/scripts.js" ></script>		
+			<!--<script src="libs/bootstrap/js/bootstrap.min.js"></script>
+			<link href="libs/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">-->
+		</head
+		<body>
+
+			include 'includes/nav.php';
+		</body>
+	</html>
+
+?> 
